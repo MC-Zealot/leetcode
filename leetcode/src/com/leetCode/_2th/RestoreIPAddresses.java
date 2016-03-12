e9e031deaf597db6602e614618290be06d311008
Zealot <yizhou.tao@yinyuetai.com>
2016-03-12 15:11:11 +0800

    leetcode

---
+++ leetcode/src/com/leetCode/_2th/RestoreIPAddresses.java
@@ -0,0 +1,24 @@
+package com.leetCode._2th;
+
+import java.util.List;
+
+/**
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+For example:
+Given "25525511135",
+
+return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+
+
+ * 
+ * @author Zealot
+ * @date 2016年3月12日 下午2:48:53 
+ *
+ */
+public class RestoreIPAddresses {
+	public List<String> restoreIpAddresses(String s) {
+        
+		return null;
+    }
+}
