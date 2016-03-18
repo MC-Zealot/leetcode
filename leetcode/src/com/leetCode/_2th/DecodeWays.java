f07c75538c4e85d48fe84a998d4ce1da36accadf
Zealot <yizhou.tao@yinyuetai.com>
2016-03-18 21:46:07 +0800

    leetcode

--- leetcode/src/com/leetCode/_2th/DecodeWays.java
+++ leetcode/src/com/leetCode/_2th/DecodeWays.java
@@ -12,6 +12,20 @@ package com.leetCode._2th;
  * 
  * The number of ways decoding "12" is 2.
  * 
+ * 定义dp[i+1]为能解码长度为i+1的string s[0:i]的方法数：
+1. dp[0] = 1，dp[1] = 0
+2. v = s[i-1]*10+s[i]：
+v<=26： dp[i+1] = dp[i] + dp[i-1]
+v>26：dp[i+1] = dp[i]
+
+corner case：有0的情况
+Y = 0：显然无法解码成h[Y]，此时只能看h[XY]是否valid：dp[i+1] = dp[i-1]
+X = 0：显然无法解码成h[XY]，此时dp[i+1] = dp[i]
+
+整理总结corner case：
+XY可以解码的条件是：9<XY<=26
+Y可以单独解码的条件是：Y != '0'
+ * 
  * @author Zealot
  * @date 2016年3月17日 上午11:38:48
  *
