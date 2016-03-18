11fee6ac49c61bf3035d170657179ebc6f23671c
Zealot <yizhou.tao@yinyuetai.com>
2016-03-18 23:07:36 +0800

    leetcode

--- leetcode/src/com/leetCode/_2th/ValidPalindrome.java
+++ leetcode/src/com/leetCode/_2th/ValidPalindrome.java
@@ -12,9 +12,24 @@ package com.leetCode._2th;
  */
 public class ValidPalindrome {
 	public boolean isPalindrome(String s) {
+		s = s.toLowerCase();
 		char[] cc = s.toCharArray();
 		int left = 0,right = cc.length-1;
 		while(left< right){
+//			System.out.println("left: "+cc[left]+", "+(cc[left]-'a'));
+			if(cc[left]-'a'>26||cc[left]-'a'<0){
+				if(!(cc[left]-'0'>=0&&cc[left]-'0'<=9)){
+					left++;
+					continue;
+				}
+			}
+			if(cc[right]-'a'>25||cc[right]-'a'<0){//不是字母
+				if(!(cc[right]-'0'>=0&&cc[right]-'0'<=9)){//不是数字
+					right--;
+					continue;
+				}
+			}
+			
 			if(cc[left] == cc[right]){
 				left++;right--;
 			}else{
@@ -27,8 +42,9 @@ public class ValidPalindrome {
 	}
 	public static void main(String[] args) {
 		ValidPalindrome vp = new ValidPalindrome();
-		System.out.println(vp.isPalindrome("121"));
-		System.out.println('c'=='c');
+//		System.out.println(vp.isPalindrome("A man, a plan, a canal: Panama"));
+		System.out.println(vp.isPalindrome("ab2a"));
+//		System.out.println('c'=='c');
 		
 	}
 	
