00751f420f21d37104ee235a9bc8f9c8594a74d8
Zealot <yizhou.tao@yinyuetai.com>
2016-04-17 14:35:31 +0800

    leetcode_2

--- leetcode/src/com/leetcode_2th/_1TwoSum.java
+++ leetcode/src/com/leetcode_2th/_1TwoSum.java
@@ -21,7 +21,8 @@ public class _1TwoSum {
 	public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         int[] res = new int[2];
-        for(int i = 0; i < nums.length; i++) {
+        int len = nums.length;
+        for(int i = 0; i < len; i++) {
             if(map.containsKey(target-nums[i])){
                 res[0] = map.get(target - nums[i]);
                 res[1] = i;
