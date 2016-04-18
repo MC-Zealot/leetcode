d1eceaeb6c9204c5723b8da435061b1af148a20f
Zealot <yizhou.tao@yinyuetai.com>
2016-04-18 18:51:28 +0800

    leetcode

---
+++ leetcode/src/com/leetcode_2th/TwoSumIIIDatastructuredesign.java
@@ -0,0 +1,27 @@
+package com.leetcode_2th;
+/**
+ * Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+For example,
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+ * 
+ * @author Zealot
+ * @date 2016年4月18日 上午11:31:03 
+ *
+ */
+public class TwoSumIIIDatastructuredesign {
+	// Add the number to an internal data structure.
+	public void add(int number) {
+	    
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {
+	    return false;
+	}
+}
