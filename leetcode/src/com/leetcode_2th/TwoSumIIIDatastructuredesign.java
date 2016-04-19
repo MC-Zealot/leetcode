ab4e4b37622a2469539589f7479a0b48c41d9cdf
Zealot <yizhou.tao@yinyuetai.com>
2016-04-19 20:55:46 +0800

    LEETCODE

--- leetcode/src/com/leetcode_2th/TwoSumIIIDatastructuredesign.java
+++ leetcode/src/com/leetcode_2th/TwoSumIIIDatastructuredesign.java
@@ -1,4 +1,10 @@
 package com.leetcode_2th;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Design and implement a TwoSum class. It should support the following operations: add and find.
 
@@ -15,13 +21,57 @@ find(7) -> false
  *
  */
 public class TwoSumIIIDatastructuredesign {
+	List<Integer> list = new ArrayList<Integer>();
+	Set<Integer> sumSet = new HashSet<Integer>();
 	// Add the number to an internal data structure.
+	/**
+	 * O(N) RUNTIME
+	 * O(N^2) SPACE
+	 * @date 2016年4月19日 下午8:54:57
+	 * @param number
+	 */
 	public void add(int number) {
-	    
+		for(Integer i: list){
+			sumSet.add(i + number);
+		}
+		list.add(number);
 	}
 
     // Find if there exists any pair of numbers which sum is equal to the value.
+	/**
+	 * O(1) RUNTIME
+	 * @date 2016年4月19日 下午8:55:03
+	 * @param value
+	 * @return
+	 */
 	public boolean find(int value) {
-	    return false;
+		return sumSet.contains(value);
+	}
+	public static void main(String[] args) {
+		TwoSumIIIDatastructuredesign t = new TwoSumIIIDatastructuredesign();
+	}
+	/**
+	 * 
+	 * 
+	 * @author Zealot
+	 * @date 2016年4月19日 下午8:54:01 
+	 *
+	 */
+	public class TwoSumIIIDatastructuredesign_2 {
+		List<Integer> list = new ArrayList<Integer>();
+		Set<Integer> sumSet = new HashSet<Integer>();
+		// Add the number to an internal data structure.
+		public void add(int number) {
+			for(Integer i: list){
+				sumSet.add(i + number);
+			}
+			list.add(number);
+		}
+		
+		// Find if there exists any pair of numbers which sum is equal to the value.
+		public boolean find(int value) {
+			return sumSet.contains(value);
+		}
 	}
 }
+
