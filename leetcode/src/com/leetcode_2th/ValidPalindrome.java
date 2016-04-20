5a08a79791107713a2aacf106d0e4ec131f29910
Zealot <yizhou.tao@yinyuetai.com>
2016-04-20 15:50:27 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/ValidPalindrome.java
+++ leetcode/src/com/leetcode_2th/ValidPalindrome.java
@@ -22,6 +22,18 @@ public class ValidPalindrome {
 		}
 		return true;
     }
+	public boolean isPalindrome2(String s) {
+		   int i = 0, j = s.length() - 1;
+		   while (i < j) {
+		      while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+		      while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+		      if (Character.toLowerCase(s.charAt(i))
+		            != Character.toLowerCase(s.charAt(j))) {
+		         return false;
+		}
+		i++; j--; }
+		   return true;
+		}
 	public static void main(String[] args) {
 		ValidPalindrome v = new ValidPalindrome();
 		System.out.println(v.isPalindrome("A man, a plan, a canal: Panama"));
