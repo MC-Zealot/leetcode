cfae0d25ea3d81ea892423b0abcc80ce9e8f5bab
Zealot <yizhou.tao@yinyuetai.com>
2016-05-13 17:29:10 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/MaximumDepthofBinaryTree.java
+++ leetcode/src/com/leetcode_2th/MaximumDepthofBinaryTree.java
@@ -1,5 +1,7 @@
 package com.leetcode_2th;
 
+import java.util.LinkedList;
+
 import com.leetCode.CommonStructure.TreeNode;
 
 /**
@@ -15,4 +17,41 @@ public class MaximumDepthofBinaryTree {
 			return 0;
 		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 	}
+	/**
+	 * 层序遍历
+	 * @date 2016年5月13日 下午5:28:43
+	 * @param root
+	 * @return
+	 */
+	public int maxDepth2(TreeNode root) {  
+	    if(root == null)  
+	        return 0;  
+	    int level = 0;  
+	    LinkedList<TreeNode> queue = new LinkedList<TreeNode>();  
+	    queue.add(root);  
+	    int curNum = 1; //num of nodes left in current level  
+	    int nextNum = 0; //num of nodes in next level  
+	    while(!queue.isEmpty())  
+	    {  
+	        TreeNode n = queue.poll();  
+	        curNum--;  
+	        if(n.left!=null)  
+	        {  
+	            queue.add(n.left);  
+	            nextNum++;  
+	        }  
+	        if(n.right!=null)  
+	        {  
+	            queue.add(n.right);  
+	            nextNum++;  
+	        }  
+	        if(curNum == 0)  
+	        {  
+	            curNum = nextNum;  
+	            nextNum = 0;  
+	            level++;  
+	        }  
+	    }  
+	    return level;  
+	}  
 }
