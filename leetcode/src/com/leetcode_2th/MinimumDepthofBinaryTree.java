ae8e4ad931c858e24799684e99b3caa072a520d5
Zealot <yizhou.tao@yinyuetai.com>
2016-05-19 17:52:11 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/MinimumDepthofBinaryTree.java
+++ leetcode/src/com/leetcode_2th/MinimumDepthofBinaryTree.java
@@ -22,4 +22,19 @@ public class MinimumDepthofBinaryTree {
 		}
 		return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
+	public static void main(String[] args) {
+		MinimumDepthofBinaryTree m = new MinimumDepthofBinaryTree();
+		TreeNode root = new TreeNode(1);
+		TreeNode root2 = new TreeNode(2);
+		TreeNode root3 = new TreeNode(3);
+		TreeNode root4 = new TreeNode(4);
+		TreeNode root5 = new TreeNode(5);
+		TreeNode root6 = new TreeNode(6);
+		root.left = root2;
+		root.right = root3;
+		root2.right = root4;
+		root3.left = root5;
+		root3.right = root6;
+		System.out.println(m.minDepth(root));
+	}
 }
