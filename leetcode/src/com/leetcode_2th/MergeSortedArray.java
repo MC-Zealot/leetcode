5c0e7e48905116fdf3a6c01e7ebf3aec8491ab1d
Zealot <yizhou.tao@yinyuetai.com>
2016-05-21 20:46:41 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/MergeSortedArray.java
+++ leetcode/src/com/leetcode_2th/MergeSortedArray.java
@@ -34,6 +34,25 @@ public class MergeSortedArray {
 		}
 		nums1 = c;
     }
+
+	public void merge2(int A[], int m, int B[], int n) {
+		if (A == null || B == null)
+			return;
+		int idx1 = m - 1;
+		int idx2 = n - 1;
+		int len = m + n - 1;
+		while (idx1 >= 0 && idx2 >= 0) {
+			if (A[idx1] > B[idx2]) {
+				A[len--] = A[idx1--];
+			} else {
+				A[len--] = B[idx2--];
+			}
+		}
+		while (idx2 >= 0) {
+			A[len--] = B[idx2--];
+		}
+	}
+
 	public static void main(String[] args) {
 		MergeSortedArray m = new MergeSortedArray();
 		int[] a = {};
