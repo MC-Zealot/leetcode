c52f5a7060f18146f123d5503ca6006de82331db
Zealot <yizhou.tao@yinyuetai.com>
2016-05-29 14:27:12 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/Clonegraph.java
+++ leetcode/src/com/leetcode_2th/Clonegraph.java
@@ -1,7 +1,9 @@
 package com.leetcode_2th;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 import com.leetCode.CommonStructure.UndirectedGraphNode;
 
@@ -33,4 +35,26 @@ public class Clonegraph {
 		}
 		return graphCopy;
 	}
+	public UndirectedGraphNode cloneGraph2(UndirectedGraphNode graph) {
+		   if (graph == null) return null;
+		   Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+		   Queue<UndirectedGraphNode> q = new LinkedList<>();
+		   q.add(graph);
+		   UndirectedGraphNode graphCopy = new UndirectedGraphNode(graph.label);
+		   map.put(graph, graphCopy);
+		   while (!q.isEmpty()) {
+		      UndirectedGraphNode node = q.poll();
+		      for (UndirectedGraphNode neighbor : node.neighbors) {
+		         if (map.containsKey(neighbor)) {
+		            map.get(node).neighbors.add(map.get(neighbor));
+		         } else {
+		            UndirectedGraphNode neighborCopy =
+		                  new UndirectedGraphNode(neighbor.label);
+		            map.get(node).neighbors.add(neighborCopy);
+		            map.put(neighbor, neighborCopy);
+		            q.add(neighbor);
+		} }
+		}
+		   return graphCopy;
+		}
 }
