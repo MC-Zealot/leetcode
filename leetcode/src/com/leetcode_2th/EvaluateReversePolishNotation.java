e0c1b8c2326d3e7b65fda2b1c3d6c7b7aa12326c
Zealot <yizhou.tao@yinyuetai.com>
2016-05-29 21:02:03 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/EvaluateReversePolishNotation.java
+++ leetcode/src/com/leetcode_2th/EvaluateReversePolishNotation.java
@@ -1,5 +1,7 @@
 package com.leetcode_2th;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -36,4 +38,47 @@ public class EvaluateReversePolishNotation {
 		
 		return stack.peek();
     }
+	
+	interface Operator {
+		int eval(int x, int y);
+	}
+
+	private static final Map<String, Operator> OPERATORS = new HashMap<String, Operator>() {
+		{
+			put("+", new Operator() {
+				public int eval(int x, int y) {
+					return x + y;
+				}
+			});
+			put("-", new Operator() {
+				public int eval(int x, int y) {
+					return x - y;
+				}
+			});
+			put("*", new Operator() {
+				public int eval(int x, int y) {
+					return x * y;
+				}
+			});
+			put("/", new Operator() {
+				public int eval(int x, int y) {
+					return x / y;
+				}
+			});
+		}
+	};
+
+	public int evalRPN2(String[] tokens) {
+		Stack<Integer> stack = new Stack<>();
+		for (String token : tokens) {
+			if (OPERATORS.containsKey(token)) {
+				int y = stack.pop();
+				int x = stack.pop();
+				stack.push(OPERATORS.get(token).eval(x, y));
+			} else {
+				stack.push(Integer.parseInt(token));
+			}
+		}
+		return stack.pop();
+	}
 }
