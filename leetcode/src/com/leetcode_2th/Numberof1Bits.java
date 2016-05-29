dc8f4915164fa14c9017332fb9cdbbdb0685bfb4
Zealot <yizhou.tao@yinyuetai.com>
2016-05-29 14:26:25 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/Numberof1Bits.java
+++ leetcode/src/com/leetcode_2th/Numberof1Bits.java
@@ -17,6 +17,16 @@ public class Numberof1Bits {
         }
         return size;
     }
+public int hammingWeight2(int n) {
+        
+        int ans = 0;
+        while (n != 0) {//可能有负数
+            ans += n & 1;
+            n >>>= 1;
+        }
+        return ans;
+    
+    }
 	public static void main(String[] args) {
 		int a = 5;
 		Numberof1Bits n = new Numberof1Bits();
