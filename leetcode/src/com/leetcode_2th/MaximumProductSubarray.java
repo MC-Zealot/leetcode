c3e0adc293d81e0764de2b6a65ad1f246625a943
Zealot <yizhou.tao@yinyuetai.com>
2016-05-31 21:10:07 +0800

    leetcode

---
+++ leetcode/src/com/leetcode_2th/MaximumProductSubarray.java
@@ -0,0 +1,15 @@
+package com.leetcode_2th;
+/**
+ * 
+ * 
+ * @author Zealot
+ * @date 2016年5月31日 下午8:58:59 
+ *
+ */
+public class MaximumProductSubarray {
+	public int maxProduct(int[] A) {
+		
+		
+		return 0;
+	}
+}
