4931658c76f619206193fc2a36366c184a97ef4d
Zealot <yizhou.tao@yinyuetai.com>
2016-05-31 14:41:16 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/UniquePaths.java
+++ leetcode/src/com/leetcode_2th/UniquePaths.java
@@ -50,20 +50,20 @@ public class UniquePaths {
 		}
 		return up[n - 1];
 	}
-	public int uniquePaths3(int m, int n) {  
-	    if(m<=0 || n<=0)  
-	        return 0;  
-	    int[] res = new int[n];  
-	    res[0] = 1;  
-	    for(int i=0;i<m;i++)  
-	    {  
-	        for(int j=1;j<n;j++)  
-	        {  
-	           res[j] += res[j-1];  
-	        }  
-	    }  
-	    return res[n-1];  
-	}  
+
+	public int uniquePaths3(int m, int n) {
+		if (m <= 0 || n <= 0)
+			return 0;
+		int[] res = new int[n];
+		res[0] = 1;
+		for (int i = 0; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				res[j] += res[j - 1];
+			}
+		}
+		return res[n - 1];
+	}
+
 	public static void main(String[] args) {
 		UniquePaths u = new UniquePaths();
 		int m = 1,n=2;
