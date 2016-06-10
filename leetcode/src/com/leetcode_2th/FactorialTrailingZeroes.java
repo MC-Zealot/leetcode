f001402f22381655f6e4678c04531a9c4cec8d7d
Zealot <yizhou.tao@yinyuetai.com>
2016-06-10 13:28:26 +0800

    LEETCODE

---
+++ leetcode/src/com/leetcode_2th/FactorialTrailingZeroes.java
@@ -0,0 +1,38 @@
+package com.leetcode_2th;
+/**
+ * Given an integer n, return the number of trailing zeroes in n!.
+n!得到的数当中，从后边数，连续有几个0
+Note: Your solution should be in logarithmic time complexity.
+ * 
+ * @author Zealot
+ * @date 2016年6月10日 下午1:00:27 
+ *
+ */
+public class FactorialTrailingZeroes {
+	public int trailingZeroes(int n) {
+		int n5 = 0;
+		for(int i = 1; i <= n; i++){
+			int a = i;
+			
+			while(a!=0 && a % 5 ==0){
+				n5++;
+				a = a / 5;
+			}
+		}
+		
+        return n5;
+    }
+	public int trailingZeroes2(int n) {
+		int x = 5;
+	    int ans = 0;
+        while (n >= x){
+        	ans += n / x;
+        	x *= 5;
+        }
+	    return ans;
+	}
+	public static void main(String[] args) {
+		FactorialTrailingZeroes f = new FactorialTrailingZeroes();
+		System.out.println(f.trailingZeroes(7));
+	}
+}
