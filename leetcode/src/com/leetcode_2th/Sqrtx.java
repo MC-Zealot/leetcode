f3888a715003a442806a66c2e66c5febebfa74cf
Zealot <yizhou.tao@yinyuetai.com>
2016-06-11 11:54:50 +0800

    LEETCODE

---
+++ leetcode/src/com/leetcode_2th/Sqrtx.java
@@ -0,0 +1,33 @@
+package com.leetcode_2th;
+
+/**
+ * Implement int sqrt(int x).
+ * 
+ * Compute and return the square root of x.
+ * 
+ * @author Zealot
+ * @date 2016年6月11日 上午11:36:49
+ *
+ */
+public class Sqrtx {
+	public int sqrt(int x) {
+		if (x < 0)
+			return -1;
+		if (x == 0)
+			return 0;
+		int l = 1;
+		int r = x / 2 + 1;
+		while (l <= r) {
+			int m = (l + r) / 2;
+			if (m <= x / m && x / (m + 1) < m + 1)
+				return m;
+			if (x / m < m) {
+				r = m - 1;
+			} else {
+				l = m + 1;
+			}
+		}
+		return 0;
+	}
+
+}
