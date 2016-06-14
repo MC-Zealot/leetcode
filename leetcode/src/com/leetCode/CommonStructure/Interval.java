68e752481b82e05db26daa3c19b381c8f522e5e0
Zealot <yizhou.tao@yinyuetai.com>
2016-06-14 09:44:34 +0800

    leetcode

---
+++ leetcode/src/com/leetCode/CommonStructure/Interval.java
@@ -0,0 +1,22 @@
+package com.leetCode.CommonStructure;
+/**
+ * 
+ * 
+ * @author Zealot
+ * @date 2016年6月13日 下午12:08:40 
+ *
+ */
+public class Interval {
+	public int start;
+	public int end;
+
+	public Interval() {
+		start = 0;
+		end = 0;
+	}
+
+	Interval(int s, int e) {
+		start = s;
+		end = e;
+	}
+}
