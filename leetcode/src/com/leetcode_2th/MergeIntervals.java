9d0745c155cd00a95de0ed3b61f9f46a6125996c
Zealot <yizhou.tao@yinyuetai.com>
2016-06-14 10:44:07 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/MergeIntervals.java
+++ leetcode/src/com/leetcode_2th/MergeIntervals.java
@@ -1,6 +1,8 @@
 package com.leetcode_2th;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -59,6 +61,31 @@ public class MergeIntervals {
 		
 		return res;
     }
+
+	public ArrayList<Interval> merge2(ArrayList<Interval> intervals) {
+		ArrayList<Interval> res = new ArrayList<Interval>();
+		if (intervals == null || intervals.size() == 0)
+			return intervals;
+		Comparator<Interval> comp = new Comparator<Interval>() {
+			@Override
+			public int compare(Interval i1, Interval i2) {
+				if (i1.start == i2.start)
+					return i1.end - i2.end;
+				return i1.start - i2.start;
+			}
+		};
+		Collections.sort(intervals, comp);
+
+		res.add(intervals.get(0));
+		for (int i = 1; i < intervals.size(); i++) {
+			if (res.get(res.size() - 1).end >= intervals.get(i).start) {
+				res.get(res.size() - 1).end = Math.max(res.get(res.size() - 1).end, intervals.get(i).end);
+			} else {
+				res.add(intervals.get(i));
+			}
+		}
+		return res;
+	}  
 	public static void main(String[] args) {
 		int num1, num2, num3;
         Scanner scanner = new Scanner(System.in);
