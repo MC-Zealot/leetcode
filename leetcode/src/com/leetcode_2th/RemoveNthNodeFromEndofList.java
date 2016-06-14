b6382ef662fee56dee86e9a4aae0b798782563c0
Zealot <yizhou.tao@yinyuetai.com>
2016-06-14 14:28:23 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/RemoveNthNodeFromEndofList.java
+++ leetcode/src/com/leetcode_2th/RemoveNthNodeFromEndofList.java
@@ -46,28 +46,27 @@ public class RemoveNthNodeFromEndofList {
 		}
 		return head;
     }
-	public ListNode removeNthFromEnd2(ListNode head, int n) {  
-	    if(head == null)  
-	        return null;  
-	    int i=0;  
-	    ListNode runner = head;  
-	    while(runner!=null && i<n)  
-	    {  
-	        runner = runner.next;  
-	        i++;  
-	    }  
-	    if(i<n)  
-	        return head;  
-	    if(runner == null)  
-	        return head.next;  
-	    ListNode walker = head;  
-	    while(runner.next!=null)  
-	    {  
-	        walker = walker.next;  
-	        runner = runner.next;  
-	    }  
-	    walker.next = walker.next.next;  
-	    return head;  
+
+	public ListNode removeNthFromEnd2(ListNode head, int n) {
+		if (head == null)
+			return null;
+		int i = 0;
+		ListNode runner = head;
+		while (runner != null && i < n) {
+			runner = runner.next;
+			i++;
+		}
+		if (i < n)
+			return head;
+		if (runner == null)
+			return head.next;
+		ListNode walker = head;
+		while (runner.next != null) {
+			walker = walker.next;
+			runner = runner.next;
+		}
+		walker.next = walker.next.next;
+		return head;
 	}  
 	public static void main(String[] args) {
 		ListNode l = new ListNode(1);
