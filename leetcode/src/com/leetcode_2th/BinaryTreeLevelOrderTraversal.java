3d33b6920e40f72171a4aa200212626fc5b6a3d2
Zealot <yizhou.tao@yinyuetai.com>
2016-06-23 10:32:33 +0800

    leetcode

---
+++ leetcode/src/com/leetcode_2th/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,35 @@
+package com.leetcode_2th;
+
+import java.util.List;
+
+import com.leetCode.CommonStructure.TreeNode;
+
+/**
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+ * 
+ * @author Zealot
+ * @date 2016年6月23日 上午10:26:16 
+ *
+ */
+public class BinaryTreeLevelOrderTraversal {
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		
+		
+		
+		return null;
+    }
+}
