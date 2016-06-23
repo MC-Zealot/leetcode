dd03195a9a19b7a2d68d3591e5adaf4567200382
Zealot <yizhou.tao@yinyuetai.com>
2016-06-23 10:25:30 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/LargestRectangleinHistogram.java
+++ leetcode/src/com/leetcode_2th/LargestRectangleinHistogram.java
@@ -20,23 +20,23 @@ return 10.
  *
  */
 public class LargestRectangleinHistogram {
-	public int largestRectangleArea(int[] height) {
-		if (height == null || height.length == 0){
+	public int largestRectangleArea(int[] heights) {
+		if (heights == null || heights.length == 0){
 			return 0;
 		}
 		int max = 0;
 		LinkedList<Integer> stack = new LinkedList<Integer>();
-		for (int i = 0; i < height.length; i++) {
-			while (!stack.isEmpty() && height[i] <= height[stack.peek()]) {
+		for (int i = 0; i < heights.length; i++) {
+			while (!stack.isEmpty() && heights[i] <= heights[stack.peek()]) {
 				int index = stack.pop();
-				int curArea = stack.isEmpty() ? i * height[index] : (i - stack.peek() - 1) * height[index];
+				int curArea = stack.isEmpty() ? i * heights[index] : (i - stack.peek() - 1) * heights[index];
 				max = Math.max(max, curArea);
 			}
 			stack.push(i);
 		}
 		while (!stack.isEmpty()) {
 			int index = stack.pop();
-			int curArea = stack.isEmpty() ? height.length * height[index] : (height.length - stack.peek() - 1) * height[index];
+			int curArea = stack.isEmpty() ? heights.length * heights[index] : (heights.length - stack.peek() - 1) * heights[index];
 			max = Math.max(max, curArea);
 		}
 		return max;
