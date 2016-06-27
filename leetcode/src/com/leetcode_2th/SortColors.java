097d70ad84fa1b24d51448192f3ba4e45a030c72
Zealot <yizhou.tao@yinyuetai.com>
2016-06-27 15:24:01 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/SortColors.java
+++ leetcode/src/com/leetcode_2th/SortColors.java
@@ -37,4 +37,41 @@ public class SortColors {
 			}
 		}
     }
+
+	public void sortColors2(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return;
+		int[] res = new int[nums.length];
+		int[] helper = new int[3];
+		for (int i = 0; i < nums.length; i++) {
+			helper[nums[i]]++;
+		}
+		for (int i = 1; i < 3; i++) {
+			helper[i] = helper[i] + helper[i - 1];
+		}
+		for (int i = nums.length - 1; i >= 0; i--) {
+			res[helper[nums[i]] - 1] = nums[i];
+			helper[nums[i]]--;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = res[i];
+		}
+	}  
+
+	public void sortColors3(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return;
+		int idx0 = 0;
+		int idx1 = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				nums[i] = 2;
+				nums[idx1++] = 1;
+				nums[idx0++] = 0;
+			} else if (nums[i] == 1) {
+				nums[i] = 2;
+				nums[idx1++] = 1;
+			}
+		}
+	}
 }
