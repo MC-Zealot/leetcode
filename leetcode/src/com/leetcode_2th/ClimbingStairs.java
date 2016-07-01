e0660e932ff1d3cd2a91321bcb29a0ee576cacae
Zealot <yizhou.tao@yinyuetai.com>
2016-07-01 15:21:25 +0800

    LEETCODE

--- leetcode/src/com/leetcode_2th/ClimbingStairs.java
+++ leetcode/src/com/leetcode_2th/ClimbingStairs.java
@@ -15,6 +15,12 @@ public class ClimbingStairs {
 		}
 		return climbStairs(n - 1) + climbStairs(n - 2);
     }
+	/**
+	 * SPACE: O(n)
+	 * @date 2016年7月1日 下午3:20:32
+	 * @param n
+	 * @return
+	 */
 	public int climbStairs2(int n) {
 		int[] a = new int[n + 1];
 		if(n == 1){
@@ -29,6 +35,26 @@ public class ClimbingStairs {
 		}
 		return a[n];
     }
+	/**
+	 * SPACE: O(1)
+	 * @date 2016年7月1日 下午3:20:54
+	 * @param n
+	 * @return
+	 */
+	public int climbStairs3(int n) {
+		int f1 = 1;
+		int f2 = 2;
+		if (n == 1)
+			return f1;
+		if (n == 2)
+			return f2;
+		for (int i = 3; i <= n; i++) {
+			int f3 = f1 + f2;
+			f1 = f2;
+			f2 = f3;
+		}
+		return f2;
+	}  
 	public static void main(String[] args) {
 		ClimbingStairs c = new ClimbingStairs();
 		System.out.println();
