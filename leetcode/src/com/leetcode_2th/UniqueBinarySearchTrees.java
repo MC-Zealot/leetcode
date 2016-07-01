f6f8ec51584ab873148f6ae9a2d30ace770fa654
Zealot <yizhou.tao@yinyuetai.com>
2016-07-01 16:10:56 +0800

    dp

--- leetcode/src/com/leetcode_2th/UniqueBinarySearchTrees.java
+++ leetcode/src/com/leetcode_2th/UniqueBinarySearchTrees.java
@@ -20,12 +20,12 @@ public class UniqueBinarySearchTrees {
 		if (n <= 0){
 			return 0;
 		}
-		int[] res = new int[n + 1];
+		int[] res = new int[n + 1];//含有i个结点的二叉查找树的数量
 		res[0] = 1;
 		res[1] = 1;
 		for (int i = 2; i <= n; i++) {
 			for (int j = 0; j < i; j++) {
-				res[i] += res[j] * res[i - j - 1];
+				res[i] += res[j] * res[i - j - 1];//选取一个结点为根，就把结点切成左右子树，以这个结点为根的可行二叉树数量就是左右子树可行二叉树数量的乘积
 			}
 		}
 		return res[n];
