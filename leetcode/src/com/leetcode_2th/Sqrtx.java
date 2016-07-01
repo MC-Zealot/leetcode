b5dd7f100377d858afd6ca186d7fc4194b3032ea
Zealot <yizhou.tao@yinyuetai.com>
2016-07-01 14:46:54 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/Sqrtx.java
+++ leetcode/src/com/leetcode_2th/Sqrtx.java
@@ -30,4 +30,27 @@ public class Sqrtx {
 		return 0;
 	}
 
+	public int sqrt2(int x) {
+		if (x < 0)
+			return -1;
+		if (x == 0)
+			return 0;
+		int l = 1;
+		int r = x / 2 + 1;
+		while (l <= r) {
+			int m = (l + r) / 2;
+			if (m == x / m)
+				return m;
+			if (x / m < m) {
+				r = m - 1;
+			} else {
+				l = m + 1;
+			}
+		}
+		return r;
+	}  
+	public static void main(String[] args) {
+		Sqrtx s =  new Sqrtx();
+		System.out.println(s.sqrt(16));
+	}
 }
