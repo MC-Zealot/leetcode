18b6949dedc006da24b5b553e178ff902316cac9
Zealot <yizhou.tao@yinyuetai.com>
2016-07-04 17:14:32 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/MaximumGap.java
+++ leetcode/src/com/leetcode_2th/MaximumGap.java
@@ -24,14 +24,14 @@ You may assume all elements in the array are non-negative integers and fit in th
  */
 public class MaximumGap {
 	public int maximumGap(int[] nums) {
-		if(nums.length<2){
+		if (nums.length < 2) {
 			return 0;
 		}
 		Arrays.sort(nums);
-        int gap=-1;  
-        for(int i=1;i<nums.length;i++){  
-            gap=Math.max(gap,nums[i]-nums[i-1]);  
-        }  
-        return gap;  
-    }
+		int gap = -1;
+		for (int i = 1; i < nums.length; i++) {
+			gap = Math.max(gap, nums[i] - nums[i - 1]);
+		}
+		return gap;
+	}
 }
