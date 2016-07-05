ce5867333225a97b3ddf7aaae3c4338e5cec99d4
Zealot <yizhou.tao@yinyuetai.com>
2016-07-05 14:11:52 +0800

    leetcode

--- leetcode/src/com/leetcode_2th/BestTimetoBuyandSellStock.java
+++ leetcode/src/com/leetcode_2th/BestTimetoBuyandSellStock.java
@@ -1,7 +1,11 @@
 package com.leetcode_2th;
 /**
  * 
- * 
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
+
+design an algorithm to find the maximum profit.
  * @author Zealot
  * @date 2016年6月4日 下午4:50:39 
  *
