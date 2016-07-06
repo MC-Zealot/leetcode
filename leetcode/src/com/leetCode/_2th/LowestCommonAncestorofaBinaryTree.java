fdc4948c7308ff24c17d5a49ebe67b0297392e64
Zealot <yizhou.tao@yinyuetai.com>
2016-07-06 10:33:01 +0800

    leetcode

--- leetcode/src/com/leetCode/_2th/LowestCommonAncestorofaBinaryTree.java
+++ leetcode/src/com/leetCode/_2th/LowestCommonAncestorofaBinaryTree.java
@@ -8,13 +8,13 @@ import com.leetCode.CommonStructure.TreeNode;
 
  According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
 
- _______3______
- /              \
- ___5__          ___1__
- /      \        /      \
- 6      _2       0       8
- /  \
- 7   4
+		  _______3______
+		 /              \
+	  ___5__          ___1__
+	 /      \        /      \
+	 6      _2       0       8
+   /  \
+   7   4
  For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
 
 
