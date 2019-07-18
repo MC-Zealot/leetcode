79ec40d666dc4d349021c0b71356afa4192b9a84
MC-Zealot <263097241@qq.com>
2019-07-18 11:12:11 +0800

    leetcode

--- leetcode/src/com/leetcode_3th/array/FindFirstandLastPositionofElementinSortedArray.java
+++ leetcode/src/com/leetcode_3th/array/FindFirstandLastPositionofElementinSortedArray.java
@@ -15,7 +15,6 @@ package com.leetcode_3th.array;
  * 
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * 
  * @author Zealot
  * @date Jul 18, 2019 10:19:18 AM
