ce8f1cc9a767964d1ee93b9b2d5e4892146f342e
MC-Zealot <263097241@qq.com>
2019-07-21 16:51:32 +0800

    leetcode

---
+++ leetcode/src/com/leetcode_3th/array/BestTimetoBuyandSellStock.java
@@ -0,0 +1,45 @@
+package com.leetcode_3th.array;
+
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ * 
+ * 注意你不能在买入股票前卖出股票。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [7,1,5,3,6,4] 
+ * 输出: 5 
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润
+ * = 6-1 = 5 。 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。 
+ * 示例 2:
+ * 输入: [7,6,4,3,1] 输出: 0 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * 
+ * 
+ * @author Zealot
+ * @date Jul 21, 2019 4:44:51 PM
+ *
+ */
+public class BestTimetoBuyandSellStock {
+	/**
+	 * O(n^2)
+	 * @date Jul 21, 2019 4:47:39 PM
+	 * @param prices
+	 * @return
+	 */
+	public int maxProfit(int[] prices) {
+		int res = 0;
+		for(int i = 0; i < prices.length; i++) {
+			for(int j = i+1; j < prices.length;j++) {
+				res = Math.max(prices[j] - prices[i], res);
+			}
+		}
+		return res;
+	}
+	public static void main(String[] args) {
+		BestTimetoBuyandSellStock b = new BestTimetoBuyandSellStock();
+		int[] prices = {7,6,4,3,1};
+		System.out.println(b.maxProfit(prices));
+	}
+}
