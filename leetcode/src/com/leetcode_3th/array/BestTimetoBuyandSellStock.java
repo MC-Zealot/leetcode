c920ad6254469c4b4aa52190951a83e0bdd45893
MC-Zealot <263097241@qq.com>
2019-07-21 17:27:18 +0800

    leetcode

--- leetcode/src/com/leetcode_3th/array/BestTimetoBuyandSellStock.java
+++ leetcode/src/com/leetcode_3th/array/BestTimetoBuyandSellStock.java
@@ -9,12 +9,9 @@ package com.leetcode_3th.array;
  * 
  * 示例 1:
  * 
- * 输入: [7,1,5,3,6,4] 
- * 输出: 5 
- * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润
- * = 6-1 = 5 。 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。 
- * 示例 2:
- * 输入: [7,6,4,3,1] 输出: 0 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * 输入: [7,1,5,3,6,4] 输出: 5 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润
+ * = 6-1 = 5 。 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。 示例 2: 输入: [7,6,4,3,1] 输出: 0 解释:
+ * 在这种情况下, 没有交易完成, 所以最大利润为 0。
  * 
  * 
  * @author Zealot
@@ -24,22 +21,53 @@ package com.leetcode_3th.array;
 public class BestTimetoBuyandSellStock {
 	/**
 	 * O(n^2)
+	 * 
 	 * @date Jul 21, 2019 4:47:39 PM
 	 * @param prices
 	 * @return
 	 */
 	public int maxProfit(int[] prices) {
 		int res = 0;
-		for(int i = 0; i < prices.length; i++) {
-			for(int j = i+1; j < prices.length;j++) {
+		for (int i = 0; i < prices.length; i++) {
+			for (int j = i + 1; j < prices.length; j++) {
 				res = Math.max(prices[j] - prices[i], res);
 			}
 		}
 		return res;
 	}
+
+	/**
+	 * dp 从每天的股价转换成每天的涨幅 然后计算最长子序列
+	 * 
+	 * @date Jul 21, 2019 5:14:42 PM
+	 * @param prices
+	 * @return
+	 */
+	public int maxProfit2(int[] prices) {
+		if(prices.length<=1) {
+			return 0;
+		}
+		int[] nums = new int[prices.length];
+		nums[0] = 0;
+		for (int i = 1; i < prices.length; i++) {
+			nums[i] = prices[i] - prices[i - 1];
+		}
+		return maxSubArray(nums);
+	}
+
+	public int maxSubArray(int[] nums) {
+		int res = nums[0];
+		int maxSoFar = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			maxSoFar = Math.max(nums[i] + maxSoFar, nums[i]);
+			res = Math.max(res, maxSoFar);
+		}
+		return res;
+	}
+
 	public static void main(String[] args) {
 		BestTimetoBuyandSellStock b = new BestTimetoBuyandSellStock();
-		int[] prices = {7,6,4,3,1};
+		int[] prices = { 7, 6, 4, 3, 1 };
 		System.out.println(b.maxProfit(prices));
 	}
 }
