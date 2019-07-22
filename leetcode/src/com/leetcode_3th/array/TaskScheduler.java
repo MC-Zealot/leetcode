ae549db59c4518b592425ac4768fb9396aef89db
MC-Zealot <263097241@qq.com>
2019-07-22 22:22:36 +0800

    leetcode

--- leetcode/src/com/leetcode_3th/array/TaskScheduler.java
+++ leetcode/src/com/leetcode_3th/array/TaskScheduler.java
@@ -1,8 +1,11 @@
 package com.leetcode_3th.array;
 
+import java.util.Arrays;
+
 /**
  * 给定一个用字符数组表示的 CPU 需要执行的任务列表。其中包含使用大写的 A - Z 字母表示的26
- * 种不同种类的任务。任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。CPU 在任何一个单位时间内都可以执行一个任务，或者在待命状态。
+ * 种不同种类的任务。任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。CPU 在任何一个单位时间内都可以执行一个任务
+ * ，或者在待命状态。
  * 
  * 然而，两个相同种类的任务之间必须有长度为 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
  * 
@@ -22,7 +25,13 @@ package com.leetcode_3th.array;
  */
 public class TaskScheduler {
 	public int leastInterval(char[] tasks, int n) {
-
-		return 0;
+		int[] cnt = new int[26];
+        for (char task : tasks) {
+            ++cnt[task - 'A'];
+        }
+        Arrays.sort(cnt);
+        int i = 25, mx = cnt[25], len = tasks.length;
+        while (i >= 0 && cnt[i] == mx) --i;
+        return Math.max(len, (mx - 1) * (n + 1) + 25 - i);
 	}
 }
