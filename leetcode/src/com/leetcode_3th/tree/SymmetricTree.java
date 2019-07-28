2457ce403fa66958110981cdcfd21ae6310e06b4
MC-Zealot <263097241@qq.com>
2019-07-28 14:24:44 +0800

    leetcode

--- leetcode/src/com/leetcode_3th/tree/SymmetricTree.java
+++ leetcode/src/com/leetcode_3th/tree/SymmetricTree.java
@@ -99,6 +99,16 @@ public class SymmetricTree {
 		}
 		return true;
 	}
+	
+	public boolean  isSymmetric2(TreeNode root) {
+        if (root==null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+	public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left==null && right==null) return true;
+        if (left!=null && right==null || left==null && right!=null || left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
 
 	public static void main(String[] args) {
 		//[1,2,2,null,3,null,3]
