ca6ade12e0305219bccc91ea624d65751b26d83f
MC-Zealot <263097241@qq.com>
2019-07-30 22:28:58 +0800

    leetcode

--- leetcode/src/com/leetcode_3th/dp/CoinChange.java
+++ leetcode/src/com/leetcode_3th/dp/CoinChange.java
@@ -25,13 +25,13 @@ package com.leetcode_3th.dp;
 public class CoinChange {
 	public int coinChange(int[] coins, int amount) {
 		int[] dp = new int[amount + 1];
-		for(int i = 1; i < dp.length; i++) {
-			dp[i] = amount+1;
+		for (int i = 1; i < dp.length; i++) {
+			dp[i] = amount + 1;
 		}
-		for(int i = 1; i <= dp.length; i++) {
-			for(int j = 0; j < coins.length; j++) {
+		for (int i = 1; i <= amount; i++) {
+			for (int j = 0; j < coins.length; j++) {
 				if (coins[j] <= i) {
-					dp[i] = Math.min(dp[i], dp[i - coins[j]]+ 1);
+					dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
 				}
 			}
 		}
