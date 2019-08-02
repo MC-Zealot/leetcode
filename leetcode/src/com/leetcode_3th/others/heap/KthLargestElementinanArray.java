7fc79a4f4b49f3d3f0bab8bbe1c166d8e6a5997e
MC-Zealot <263097241@qq.com>
2019-08-02 12:53:27 +0800

    leetcode

--- leetcode/src/com/leetcode_3th/others/heap/KthLargestElementinanArray.java
+++ leetcode/src/com/leetcode_3th/others/heap/KthLargestElementinanArray.java
@@ -27,6 +27,69 @@ public class KthLargestElementinanArray {
 		Arrays.sort(nums);
 		return nums[nums.length - k];
 	}
+	/**
+	 * avr: O(n) worst O(n^2)
+	 * @date 2016年7月3日 下午11:10:46
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public int findKthLargest3(int[] nums, int k) {
+        return quickSelect(nums, k - 1, 0, nums.length - 1);
+    }
+    
+	/**
+	 * 跟快速排序一个思路。先取一个枢纽值，将数组中小于枢纽值的放在左边，大于枢纽值的放在右边，具体方法是用左右两个指针，如果他们小于枢纽值则将他们换到对面，
+	 * 
+	 * 一轮过后记得将枢纽值赋回分界点。如果这个分界点是k，说明分界点的数就是第k个数。
+	 * 
+	 * 否则，如果分界点大于k，则在左半边做同样的搜索。如果分界点小于k，则在右半边做同样的搜索。
+	 * @date Aug 2, 2019 12:48:05 PM
+	 * @param arr
+	 * @param k
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	private int quickSelect(int[] arr, int k, int left, int right) {
+		int pivot = arr[(left + right) / 2];
+		int orgL = left, orgR = right;
+		while (left <= right) {
+			// 从右向左找到第一个小于枢纽值的数
+			while (arr[left] > pivot) {
+				left++;
+			}
+			// 从左向右找到第一个大于枢纽值的数
+			while (arr[right] < pivot) {
+				right--;
+			}
+			// 将两个数互换
+			if (left <= right) {
+				swap(arr, left, right);
+				left++;
+				right--;
+			}
+		}
+		// 最后退出的情况应该是右指针在左指针左边一格
+		// 这时如果右指针还大于等于k，说明kth在左半边
+		if (orgL < right && k <= right) {
+			return quickSelect(arr, k, orgL, right);
+		}
+		// 这时如果左指针还小于等于k，说明kth在右半边
+		if (left < orgR && k >= left) {
+			return quickSelect(arr, k, left, orgR);
+		}
+			
+		return arr[k];
+
+	}
+    
+    private void swap(int[] arr, int idx1, int idx2){
+        int tmp = arr[idx1] + arr[idx2];
+        arr[idx1] = tmp - arr[idx1];
+        arr[idx2] = tmp - arr[idx2];
+    
+    }
 	public static void main(String[] args) {
 		KthLargestElementinanArray k = new KthLargestElementinanArray();
 		int[] nums= {3,2,3,1,2,4,5,5,6};
