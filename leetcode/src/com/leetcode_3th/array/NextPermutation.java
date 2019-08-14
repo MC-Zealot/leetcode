bc4908f362a560f847ade5bfc5cb3e092744373c
MC-Zealot <263097241@qq.com>
2019-08-14 15:15:53 +0800

    leetcode

--- leetcode/src/com/leetcode_3th/array/NextPermutation.java
+++ leetcode/src/com/leetcode_3th/array/NextPermutation.java
@@ -47,6 +47,36 @@ public class NextPermutation {
 		System.out.println(Arrays.toString(nums));
 	}
 
+	/**
+	 * 
+	 * @date Aug 14, 2019 3:15:41 PM
+	 * @param nums
+	 */
+	public void nextPermutation2(int[] nums) {
+		int len = nums.length;
+		int start = 0;
+		OK:
+		for (int i = len - 1; i >= 0; i--) {
+			for (int j = len - 1; j >= i; j--) {
+				if (nums[j] > nums[i]) {
+					int tmp = nums[j];
+					nums[j] = nums[i];
+					nums[i] = tmp;
+					start = i + 1;
+					break OK;
+				}
+			}
+		}
+		int[] tmp_nums = new int[len - start];
+		int s = start;
+		for (int i = 0; i < tmp_nums.length; i++) {
+			tmp_nums[i] = nums[start++];
+		}
+		Arrays.sort(tmp_nums);
+		for (int i = 0; s < len; s++, i++) {
+			nums[s] = tmp_nums[i];
+		}
+	}
 	public static void main(String[] args) {
 		NextPermutation np = new NextPermutation();
 //		int[] a = {1,3,2};
