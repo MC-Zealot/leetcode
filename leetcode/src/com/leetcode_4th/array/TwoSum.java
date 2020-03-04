87f822d7952bc7be451029ac210ae07310fd2b57
MC-Zealot <263097241@qq.com>
2020-03-04 13:33:33 +0800

    lc & web test

--- leetcode/src/com/leetcode_4th/array/TwoSum.java
+++ leetcode/src/com/leetcode_4th/array/TwoSum.java
@@ -1,6 +1,9 @@
 package com.leetcode_4th.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,4 +39,57 @@ public class TwoSum {
 		
 		return ret;
     }
+	/**
+	 * 
+	 * @param a
+	 * @param n
+	 * @return
+	 * Created on: Mar 4, 2020 1:32:48 PM
+	 */
+	public List<List<Integer>> twoSum2(int[] a, int n){
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+	  	if(a.length==0){
+	    	return res;
+	    }
+	  	Arrays.sort(a);//对数组排序，解决可能重复元素的问题
+	  	Map<Integer, Integer> map = new HashMap<Integer, Integer>();//<值，下标>，
+	  	Map<Integer, Integer> value_count = new HashMap<Integer, Integer>(); //值，计数
+	  	//计算value_count
+	  	for(int i = 0 ; i < a.length; i++){
+	    	if(value_count.containsKey(a[i])){
+	        	int count = value_count.get(a[i]);
+	          	value_count.put(a[i], count+1);
+	        }else{
+	         	value_count.put(a[i],1); 
+	        }
+	    }
+	  
+	  
+	  	for(int i = 0; i < a.length; i++){
+	    	if(map.containsKey(n - a[i])){
+	        	List<Integer> pair = new ArrayList<Integer>();
+	          	pair.add(map.get(n - a[i]));
+	          	pair.add(i);
+	          	int tmp1 = value_count.get(map.get(n - a[i]));
+	          	int tmp2 = value_count.get(i);
+	          	int x = tmp1 + tmp2;
+	          	for(int j = 0; j < x; j++){//如果出现多个重复现象
+	            	res.add(new ArrayList<Integer>(pair));
+	            }
+	          	
+	        }
+	      	map.put(a[i],i);
+	    }
+	  	return res;
+
+	}
+	public static void main(String[] args) {
+		TwoSum t = new TwoSum();
+		int[] a = {1,1,2,2,1};
+		int n = 3;
+		List<List<Integer>> res = t.twoSum2(a, n);
+		for(int i = 0; i< res.size(); i++) {
+			
+		}
+	}
 }
