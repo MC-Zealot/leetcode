c091c1cf36a82f73935e8d9ad6eac72ae7c91d73
taoyizhou <263097241@qq.com>
2021-08-01 17:45:05 +0800

    lc

--- com/oj_5th/leetcode/editor/cn/P25ReverseNodesInKGroup.java
+++ com/oj_5th/leetcode/editor/cn/P25ReverseNodesInKGroup.java
@@ -56,7 +56,12 @@
 // 👍 1208 👎 0
 
 
-package com.oj_5th.leetcode.editor.cn;
+package oj_5th.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /*
 *   [25] reverse-nodes-in-k-group
 *   2021-07-31 13:23:32
@@ -80,7 +85,39 @@ public class P25ReverseNodesInKGroup{
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-
+        if (head == null) {
+            return head;
+        }
+        List<Stack> list = new ArrayList<Stack>();
+        Stack s = new Stack();
+        int index = 0;
+        while (head != null) {
+            s.add(head.val);
+            head = head.next;
+            index++;
+            if (index % k == 0 && s.size() > 0) {
+                list.add(s);
+                s = new Stack();
+            }
+        }
+        if (s.size() > 0) {
+            Stack lastStack = new Stack();
+            while (s.size() > 0) {
+                lastStack.add(s.pop());
+            }
+            list.add(lastStack);
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        for (int i = 0; i < list.size(); i++) {
+            Stack<Integer> ss = list.get(i);
+            while (ss.size() > 0) {
+                ListNode tmp = new ListNode(ss.pop());
+                p.next = tmp;
+                p = p.next;
+            }
+        }
+        return dummy.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
